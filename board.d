deb898a31e06f7cc079a101796eaf62096c02de7
ethanchan11 <eethan.chan11@gmail.com>
2022-12-03 16:03:48 -0500

    without .d files lol

--- board.d
+++
@@ -1,5 +0,0 @@
-board.o: board.cc board.h Levels/level.h Blocks/block.h subject.h \
- Levels/lvl0.h Levels/level.h Levels/lvl1.h Levels/lvl2.h Levels/lvl3.h \
- Levels/lvl4.h Blocks/iBlock.h Blocks/block.h Blocks/jBlock.h \
- Blocks/lBlock.h Blocks/oBlock.h Blocks/sBlock.h Blocks/tBlock.h \
- Blocks/zBlock.h Blocks/cube.h
